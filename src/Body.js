ac66bc9988a601fecc21a283dce22459762b3f79
brayden-gg <bgoldsteingelb@gmail.com>
2024-02-03 14:38:41 -0500

    moving circle!

---
+++ src/Body.js
@@ -0,0 +1,57 @@
+class Body {
+    constructor(options) {
+        this.pos = createVector(options.x || 0, options.y || 0);
+        this.vel = createVector(options.velX || 0, options.velY || 0);
+        this.angle = options.angle || 0;
+        this.mass = options.mass || 0;
+        this.forces = [];
+        this.angularVel = options.angularVel || 0;
+        this.angularAcc = options.angularAcc || 0;
+        this.torques = [];
+        this.inertia = options.inertia || 0;
+    }
+
+    get x() {
+        return this.pos.x;
+    }
+
+    get y() {
+        return this.pos.y;
+    }
+
+    set x(val) {
+        this.pos.x = val;
+    }
+
+    set y(val) {
+        this.pos.y = val;
+    }
+
+    show() {
+        ellipse(this.x, this.y, 50, 50);
+        return this;
+    }
+
+    clearForces() {
+        this.forces = [];
+    }
+
+    addForce(force) {
+        this.forces.push(force);
+        return this;
+    }
+
+    update(dt) {
+        let F = createVector();
+
+        for (let force of this.forces) {
+            F.add(force);
+        }
+
+        let acc = p5.Vector.div(F, this.mass);
+        this.vel.add(p5.Vector.mult(acc, dt));
+        this.pos.add(p5.Vector.mult(this.vel, dt));
+
+        return this;
+    }
+}
--- src/game.js
+++ src/game.js
@@ -1,7 +1,15 @@
+let ship;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
+
+    ship = new Body({ mass: 10 });
 }
 
 function draw() {
     background(0);
+    // rect(100, 100, 100, 100);
+    ship.addForce(createVector(10, 5));
+    ship.update(1 / 60);
+    ship.show();
 }
