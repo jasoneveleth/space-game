6982a8a63c89b044625f7ff461857eaa9cf99d17
Zhengyu Zou <113187557+zzou9@users.noreply.github.com>
2024-02-04 10:59:08 -0500

    .

--- src/levels.js
+++ src/levels.js
@@ -5,18 +5,18 @@ function level_setup() {
 Use arrow keys to turn and space bar for thrust`,
             planetList: [],
             ship: { x: 100, y: 300, mass: 10, velY: 0, angle: Math.PI / 4 },
-            home: { x: 860, y: windowHeight / 2, mass: 2000, img: sprites.home, r: 25 },
+            home: { x: 860, y: windowHeight / 2, mass: 2000, img: sprites.home, r: 50 },
         },
 
         {
             text: "Avoid other planets",
             planetList: [
-                { x: 20, y: 300, mass: 1000, img: sprites.planet[0] },
+                { x: 20, y: 300, mass: 2000, img: sprites.planet[0] },
                 { x: 400, y: 300, mass: 10000, img: sprites.planet[1] },
                 { x: 300, y: 400, mass: 10000, img: sprites.planet[1] },
             ],
             ship: { x: 100, y: 300, mass: 10, velY: 0 },
-            home: { x: 860, y: windowHeight / 2, mass: 2000, img: sprites.home, r: 25 },
+            home: { x: 860, y: windowHeight / 2, mass: 2000, img: sprites.home, r: 50 },
         },
     ];
 }
