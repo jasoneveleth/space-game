ae3cfd94d01c1cfb442f58feb192d9554adcd32a
Zhengyu Zou <113187557+zzou9@users.noreply.github.com>
2024-02-04 11:47:10 -0500

    .

--- src/levels.js
+++ src/levels.js
@@ -2,6 +2,19 @@ function level_setup() {
     return [
         { planetList: [{ x: WIDTH / 2 - 100, y: HEIGHT - 30, velX: -300, velY: 10, img: sprites.planet[0] }] },
         {
+            text: "Far from home",
+            planetList: [
+                { x: 100, y: 600, velX: -0.05, mass: -10000, img: sprites.planet[1]}, 
+                { x: 20, y: 150, velX: -0.05, mass: -10000, img: sprites.planet[1]}, 
+                { x: 300, y: 500, velX: -0.05, velY: -0.05, mass: 10000, img: sprites.planet[0] },
+                { x: 450, y: 350, velX: 0.05, velY: 0.05, mass: 10000, img: sprites.planet[0] },
+                { x: 670, y: 300, velX: -0.05, mass: -10000, img: sprites.planet[1] },
+                { x: 600, y: 600, velX: -0.05, mass: -10000, img: sprites.planet[1] }
+            ],
+            ship: { x: 100, y: 300, mass: 10, velY: 0 },
+            home: { x: 860, y: windowHeight / 2, mass: 2000, img: sprites.home, r: 50 },
+        },
+        {
             text: `Try to get home! 
 Use arrow keys to turn and space bar for thrust.`,
             planetList: [],
@@ -41,7 +54,7 @@ Use arrow keys to turn and space bar for thrust.`,
             home: { x: 860, y: windowHeight / 2, mass: 2000, img: sprites.home, r: 50 },
         },
         {
-            text: "Negative Gravity",
+            text: "Negative gravity?",
             planetList: [
                 { x: 670, y: 300, velX: -0.05, mass: -5000, img: sprites.planet[1] },
                 { x: 600, y: 600, velX: -0.05, mass: -5000, img: sprites.planet[1] },
